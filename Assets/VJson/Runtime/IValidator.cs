53aaf27690ed01ff86de1e06721ba601d21c8ef7
yutopp <yutopp@gmail.com>
2019-02-11 17:36:46 +0900

    Licensed by BSL 1.0

--- Assets/VJson/Runtime/IValidator.cs
+++ Assets/VJson/Runtime/IValidator.cs
@@ -1,4 +1,11 @@
-﻿namespace VJson
+﻿//
+// Copyright (c) 2019- yutopp (yutopp@gmail.com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at  https://www.boost.org/LICENSE_1_0.txt)
+//
+
+namespace VJson
 {
 	public interface IValidator
 	{
