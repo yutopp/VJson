70ba18481f292bc9bac3355c8157b9aa18666330
yutopp <yutopp@gmail.com>
2021-10-03 01:48:23 +0900

    Derive PreserveAttribute

--- Packages/net.yutopp.vjson/Runtime/Schema/JsonSchema.cs
+++ Packages/net.yutopp.vjson/Runtime/Schema/JsonSchema.cs
@@ -15,7 +15,7 @@ namespace VJson.Schema
     // TODO: Divide attributes and models.
     [Json(ImplicitConstructable = true)]
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Field, Inherited = false)]
-    public class JsonSchemaAttribute : Attribute
+    public class JsonSchemaAttribute : PreserveAttribute
     {
         #region Core
         [JsonField(Name = "$schema", Order = -10)]
